77aa7e8765f1dfca82accbcd5780238948b32c1a
root <root@dc205bcd5c1c.mylabserver.com>
2025-03-14 16:05:56 +0000

    ec2

--- resources.tf
+++ resources.tf
@@ -101,7 +101,7 @@ resource "aws_security_group" "nginx_sg" {
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type          = "t2.micro"
-  subnet_id              = module.main.aws_subnet.public[0]
+  subnet_id              = module.main.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
   user_data = <<EOF
