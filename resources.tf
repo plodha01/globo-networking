9ef8b1d1d9f1242fdff8e9c8d51cd927e282460d
root <root@dc205bcd5c1c.mylabserver.com>
2025-03-14 15:46:29 +0000

    new ec2

--- resources.tf
+++ resources.tf
@@ -17,7 +17,8 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    env = "test"
+    env     = "test"
+    billing = "pun"
   }
 }
 
@@ -62,3 +63,56 @@ resource "aws_security_group" "ingress" {
   tags_all               = {}
   vpc_id                 = module.main.vpc_id
 }
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ssm_parameter" "amzn2_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+# SECURITY GROUPS #
+# Nginx security group 
+resource "aws_security_group" "nginx_sg" {
+  name   = "nginx_sg"
+  vpc_id = module.main.vpc_id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+}
+
+# INSTANCES #
+resource "aws_instance" "nginx1" {
+  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type          = var.instance_type
+  subnet_id              = module.main.aws_subnet.public[0]
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
+  user_data = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+EOF
+
+  tags = local.common_tags
+
+}
+
